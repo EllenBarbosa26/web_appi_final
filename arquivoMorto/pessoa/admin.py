15d4afb317889d1cd2f24a050d3311e962c5349c
Vinício Couto <110042493+VinicioCouto@users.noreply.github.com>
2025-03-22 16:32:32 -0300

    teste

--- arquivoMorto/pessoa/admin.py
+++ arquivoMorto/pessoa/admin.py
@@ -14,14 +14,4 @@ class PessoaAdmin(admin.ModelAdmin):
         }),
     )
 
-@admin.register(Endereco)
-class EnderecoAdmin(admin.ModelAdmin):
-    list_display = ("rua", "bairro", "cidade", "estado", "cep")  # Exibe os campos na listagem
-    search_fields = ("rua", "bairro", "cidade", "estado", "cep")  # Permite busca por rua, bairro, cidade, estado e cep
-    ordering = ("cidade", "bairro", "rua")  # Ordena primeiro por cidade, depois bairro e rua
 
-@admin.register(ArquivoPDF)
-class ArquivoPDFAdmin(admin.ModelAdmin):
-    list_display = ("pessoa", "pdf")  # Exibe pessoa e arquivo PDF
-    search_fields = ("pessoa__nome",)  # Permite buscar pelo nome da pessoa associada ao arquivo
-    ordering = ("pessoa__nome",)  # Ordena os arquivos pelo nome da pessoa associada
